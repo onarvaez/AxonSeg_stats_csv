0c84dd07c2070c950cdad5b95b65317cbb2d1a0a
Omar Narvaez <om.narv@gmail.com>
2018-03-23 16:49:35 -0600

    Delete save_avf_mvf.m

--- save_avf_mvf.m
+++
@@ -1,6 +0,0 @@
-
-	load('avf_mvf.mat')
-    csvwrite('avf_cat.csv', avf_cat)
-    csvwrite('axon_pixel_area_cat.csv', axon_area_cat)
-    csvwrite('mvf_cat.csv', mvf_cat)
-    csvwrite('myelin_pixel_area_cat.csv', myelin_area_cat)
