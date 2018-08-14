204379e6662f7cee4736e4d463eccd06e282208d
Omar Narvaez <om.narv@gmail.com>
2018-08-13 19:59:04 -0500

    Update loop_axonlist_mvf_avf.m

--- loop_axonlist_mvf_avf.m
+++ loop_axonlist_mvf_avf.m
@@ -6,5 +6,5 @@ files = {'axonlist_full_image.mat'}
 for index = 1 : length(files)
 	thisfile = files{index}; 
 	fprintf(1,'file name for index %d is %s\n',index,thisfile)
-	axonlist_stats_mvf_avf_mozaic(thisfile)
+	axonlist_der_files_mozaic(thisfile)
 end 
