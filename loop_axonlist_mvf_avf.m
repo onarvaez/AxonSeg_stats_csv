d8e875318eeae9564a4f34c3a6e444c51b4e7670
Omar Narvaez <om.narv@gmail.com>
2018-08-13 19:56:14 -0500

    Update loop_axonlist_mvf_avf.m

--- loop_axonlist_mvf_avf.m
+++ loop_axonlist_mvf_avf.m
@@ -1,7 +1,5 @@
 
-files = {'/misc/torrey/onarvaez/M-isq/isq/isq04/right_intact/analisis/1/img_t1_z1_c1_Segmentation/axonlist_full_image.mat'}
-         %'/misc/torrey/onarvaez/M-isq/ct/PQR-ISQ-01/PQR-ISQ-01_b/INB-00-0041/img_t1_z1_c1_Segmentation/axonlist_full_image.mat'...
-         %'/misc/torrey/onarvaez/M-isq/ct/PQR-ISQ-01/PQR-ISQ-01_b/INB-00-0043/img_t1_z1_c1_Segmentation/axonlist_full_image.mat'...
+files = {'axonlist_full_image.mat'}
      
    
 
